55a36bae9de173095071fb1958e2d8da0b503fd0
jc4653 <jc4653@drexel.edu>
2023-02-11 17:44:16 +0000

    Replace cardioSessions.py

--- cardioSessions.py
+++ cardioSessions.py
@@ -1,22 +1,21 @@
 def cardioSessionInfo():
   try:
-    userWorkout = str(input("Enter your workout: "))
+    userAge = float(input("Enter your age: "))
+    userWeight = float(input("Enter your weight (pounds): "))
+    userHeartRate = float(input("Enter your heart rate: "))
     workoutTime = float(input("Enter your workout duration (minutes): "))
-    workoutDistance = int(input("Enter your workout distance (miles): "))
-    caloriesBurned =  int(input("Enter amount of calories burned: "))
+    caloriesBurned =  float(((userAge * 0.2017) - (userWeight * 0.09036) + (userHeartRate * 0.6309) - 55.0969)*(workoutTime/4.148)) 
     userNotes =  str(input("Notes: "))
   except: 
     print("Input is invalid. Please try again.")
     cardioSessionInfo()
 
-  if workoutTime <= 0 or workoutDistance <= 0 or caloriesBurned <= 0:
+  if workoutTime <= 0 or userAge <= 0 or userWeight <= 0 or userHeartRate <= 0:
     print("One or more input is invalid. Please try again.")
     cardioSessionInfo()
 
-  print(f"Workout: {userWorkout}")
-  print(f"Duration: {workoutTime:.2f} minutes")
-  print(f"Distance: {workoutDistance} miles")
-  print(f"Calories Burned: {caloriesBurned}")
+  print(f"\nDuration: {workoutTime:.2f} minutes")
+  print(f"Calories Burned: {caloriesBurned:.2f}")
   print(f"Notes: {userNotes}")
 
 cardioSessionInfo()
