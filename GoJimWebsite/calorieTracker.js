310ec8e33afeee1ba0ffe6a8419eca560873f04c
Amaro Truong <at3499@drexel.edu>
2023-05-14 20:02:42 +0000

    Update calorieTracker.js(Implemented an auto-fill system for common foods)

--- GoJimWebsite/calorieTracker.js
+++ GoJimWebsite/calorieTracker.js
@@ -8,4 +8,34 @@ function addItem(event) {
 	itemList.appendChild(listItem);
 	document.getElementById('food').value = '';
 	document.getElementById('calories').value = '';
+
+  
+}
+// array of foods 
+let commonFoods = ["Apple", "Banana", "Eggs", "Salad", "Halal", "Kims Dragon"];
+
+// function to show food suggestions based on user input
+function showFoodSuggestions(value) {
+  let foodSuggestions = document.getElementById("foodSuggestions");
+  foodSuggestions.innerHTML = "";
+
+  if (!value) {
+    return;
+  }
+
+  // filter foods that start with the provided character
+  let filteredFoods = commonFoods.filter(food => food.toLowerCase().startsWith(value.toLowerCase()));
+
+  for (let i = 0; i < filteredFoods.length; i++) {
+    let div = document.createElement("div");
+    div.innerHTML = filteredFoods[i];
+
+    // autofill when clicked 
+    div.onclick = function() {
+      document.getElementById("food").value = this.innerText;
+      foodSuggestions.innerHTML = "";
+    };
+
+    foodSuggestions.appendChild(div);
+  }
 }
