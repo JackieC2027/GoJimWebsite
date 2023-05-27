5260a6b840005d6fd24d6150535b72f73ba552ba
jc4653 <jc4653@drexel.edu>
2023-05-27 22:17:20 +0000

    Updated all files within our file to incorporate optimization; created single-line executions for HTML pages and decrease sizing of files by 50% for all files.

--- GoJimWebsite/Calc.js
+++ GoJimWebsite/Calc.js
@@ -1,48 +1,9 @@
-function getBMI(){
-  var weight = document.getElementById("bmiWeight").value;
-  var heightCM = document.getElementById("bmiHeight").value;
-  var heightM = heightCM / 100;
-  var bmi = weight / (heightM ** 2);
-  document.getElementById("bmiValue").textContent = "Your BMI is: " + bmi.toFixed(2);
-}
-
-function get1RM(){
-  var weight = document.getElementById("rmWeight").value;
-  var reps = document.getElementById("rmReps").value;
-  var x = reps * 2.5
-  var percent = (100 - x) / 100;
-  var max = weight / percent
-  document.getElementById("OneRMValue").textContent = "Your One Rep Max is: " + max + "kg";
-}
-
-function getTDEE(){
-  var gender = document.getElementById("tdeeSex").value;
-  var weight = document.getElementById("tdeeWeight").value;
-  var height = document.getElementById("tdeeHeight").value;
-  var age = document.getElementById("tdeeAge").value;
-  var activity = document.getElementById("tdeeActivity").value;
-  var bmr = 0.00;
-  var tdee = 0.00
-  if (gender == "male"){
-  bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5;
-  }
-  else {
-  bmr = (10 * weight) + (6.25 * height) - (5 * age) - 161;
-  }
-  if (activity = "sed"){
-    tdee = bmr * 1.2
-  }
-  else if (activity = "light"){
-    tdee = bmr * 1.375
-  }
-  else if (activity = "mod"){
-    tdee = bmr * 1.55
-  }
-  else if (activity = "very"){
-    tdee = bmr * 1.725
-  }
-  else if (activity = "extra"){
-    tdee = bmr * 1.9
-  }
-  document.getElementById("tdeeValue").textContent = "Your TDEE is: " + tdee + " calories per day";
-}
+import{GLTFLoader}from 'three/addons/loaders/GLTFLoader.js';const renderer=new THREE.WebGLRenderer();renderer.setSize(window.innerWidth,window.innerHeight);document.body.appendChild(renderer.domElement);const camera=new THREE.PerspectiveCamera(45,window.innerWidth/window.innerHeight,0.1,1000);camera.position.z=5;const scene=new THREE.Scene();const loader=new THREE.GLTFLoader();let model;loader.load('male.glb',(gltf)=>{model=gltf.scene;scene.add(model)});const light=new THREE.AmbientLight(0xffffff);scene.add(light);let isDragging=!1;let previousMousePosition={x:0,y:0};function onMouseDown(event){isDragging=!0}
+function onMouseMove(event){if(isDragging){const deltaMove={x:event.offsetX-previousMousePosition.x,y:event.offsetY-previousMousePosition.y};const deltaRotationQuaternion=new function getBMI(){var weight=document.getElementById("bmiWeight").value;var heightCM=document.getElementById("bmiHeight").value;var heightM=heightCM/100;var bmi=weight/(heightM**2);document.getElementById("bmiValue").textContent="Your BMI is: "+bmi.toFixed(2)}
+function get1RM(){var weight=document.getElementById("rmWeight").value;var reps=document.getElementById("rmReps").value;var x=reps*2.5
+var percent=(100-x)/100;var max=weight/percent
+document.getElementById("OneRMValue").textContent="Your One Rep Max is: "+max+"kg"}
+function getTDEE(){var gender=document.getElementById("tdeeSex").value;var weight=document.getElementById("tdeeWeight").value;var height=document.getElementById("tdeeHeight").value;var age=document.getElementById("tdeeAge").value;var activity=document.getElementById("tdeeActivity").value;var bmr=0.00;var tdee=0.00
+if(gender=="male"){bmr=(10*weight)+(6.25*height)-(5*age)+5}else{bmr=(10*weight)+(6.25*height)-(5*age)-161}
+if(activity="sed"){tdee=bmr*1.2}else if(activity="light"){tdee=bmr*1.375}else if(activity="mod"){tdee=bmr*1.55}else if(activity="very"){tdee=bmr*1.725}else if(activity="extra"){tdee=bmr*1.9}
+document.getElementById("tdeeValue").textContent="Your TDEE is: ";tdee+" calories per day"}}}
--- GoJimWebsite/tipsscript.js
+++ GoJimWebsite/tipsscript.js
@@ -1,11 +1,1 @@
-const images = document.querySelectorAll('.image');
-images.forEach((image) => {
-  image.addEventListener('mouseenter', (event) => {
-    const description = event.target.parentNode.querySelector('.description');
-    description.style.display = 'block';
-  });
-  image.addEventListener('mouseleave', (event) => {
-    const description = event.target.parentNode.querySelector('.description');
-    description.style.display = 'none';
-  });
-});
+const images=document.querySelectorAll(".image");images.forEach((e=>{e.addEventListener("mouseenter",(e=>{e.target.parentNode.querySelector(".description").style.display="block"})),e.addEventListener("mouseleave",(e=>{e.target.parentNode.querySelector(".description").style.display="none"}))}));
